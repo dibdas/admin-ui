7f6e803d12025da10b36104babdccbc3cab5d48d
dibdas <dibyandu08das@gmail.com>
2023-02-28 00:46:26 +0530

    modified

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,1 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+export const fetchData = createAsyncThunk("fetch/users", async () => {});
+const userSlice = createSlice({
+  name: "userSlice",
+});
