e81d7537fd1079183c0bce44c9f73cdb151fa764
dibdas <dibyandu08das@gmail.com>
2023-03-03 00:55:27 +0530

    modified

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from "react";
 import Lists from "../../components/Lists/Lists";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../../redux/slices/userSlice";
+import Search from "../../components/search/Search";
 // import Pagination from "../../components/pagination/Pagination";
+import "./Home.scss";
 
 function Home(props) {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.userReducer.users);
-
+  const [query, setQuery] = useState(null);
+  console.log(query);
   useEffect(() => {
     dispatch(fetchData());
   }, []);
+
   return (
-    <div>
-      <Lists users={users} />
+    <div className="home">
+      <Search setQuery={setQuery} />
+      <Lists users={users} query={query} />
       {/* <Pagination itemsPerPage={10} pageCount={pageCount} users={users} /> */}
     </div>
   );
