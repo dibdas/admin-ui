d5b699fdf083189c093921a6e49c28af2a6d76b1
dibdas <dibyandu08das@gmail.com>
2023-03-02 13:47:54 +0530

    added

---
+++ src/components/search/Search.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import "./Search.scss";
+
+function Search({ setQuery }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  // const query = searchParams.get("query");
+  // const allo = searchParams.get("age");
+  // console.log(age + allo);
+  // console.log(searchParams.get("age"));
+  const navigate = useNavigate();
+  function handleSubmit(event) {
+    event.preventDefault();
+    setQuery(() => searchParams.get("query"));
+  }
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="search"
+          // onClick={() => navigate("/filterdata")}
+          onChange={(event) => setSearchParams({ query: event.target.value })}
+        />
+        <button onClick={handleSubmit}>submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Search;
