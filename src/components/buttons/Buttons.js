80f51b42d665aaa6b1dfcd8412fad25781b64532
dibdas <dibyandu08das@gmail.com>
2023-03-02 22:43:58 +0530

    modified

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiEditAlt } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
 import "./Buttons.scss";
 
 function Buttons({ id }) {
+  const [Id, setId] = useState(null);
+  // console.log(Id);
+  function handleEdit(event) {
+    // console.log(event.target.value);
+    setId(event.target.value);
+    console.log(Id);
+  }
+  function handleDelete(event) {
+    // console.log(event.target.value);
+  }
   return (
     <div className="buttons">
-      <div className="edit">
+      <div className="edit" onClick={() => setId(id)}>
         <BiEditAlt />
       </div>
       <div className="delete">
-        <AiOutlineDelete />
+        <AiOutlineDelete onClick={handleDelete} />
       </div>
     </div>
   );
