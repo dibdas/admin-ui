a75beed68065c6bb376e57cb059d926e03411d1b
dibdas <dibyandu08das@gmail.com>
2023-03-04 01:14:30 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import Lists from "./components/Lists/Lists";
 import Home from "./pages/home/Home";
 
