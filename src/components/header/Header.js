989a0ab7a300210cb1b85d9e4d9da21beb540588
dibdas <dibyandu08das@gmail.com>
2023-03-04 23:49:44 +0530

    modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import "./Header.scss";
 
-function Header(props) {
+function Header({ onSelectCheckBox, pageNumber }) {
+  console.log(pageNumber);
   const [isChecked, setIsChecked] = useState(false);
   return (
     <div className="header">
       <div className="headings-checkbox">
         <div className="checkbox">
-          <input type="checkbox" />
+          <input type="checkbox" onChange={() => onSelectCheckBox(true)} />
         </div>
         <div className="headings">
           <div className="name">Name</div>
